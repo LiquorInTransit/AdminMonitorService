b2ff07f4b91a399331374c219acfa3cc41648695
Christopher Peckover <christopher.peckover@gmail.com>
2017-08-26 17:27:55 -0400

    Initial Commit

---
+++ src/main/java/com/gazorpazorp/AdminMonitorService/AdminMonitorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gazorpazorp.AdminMonitorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminMonitorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminMonitorServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gazorpazorp/AdminMonitorService/AdminMonitorServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gazorpazorp.AdminMonitorService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminMonitorServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
